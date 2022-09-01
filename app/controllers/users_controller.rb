4b0f464862a9e80a61ea02a15d42372de569dc60
Hiro-Kanetake <hiro_k@live.com>
2022-09-01 22:16:11 +0900

    feat: Create controller for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     @users = User.all
   end
@@ -8,5 +10,24 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: "Invalid Access"
+    end 
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "Successfully updated your account information!"
+    else
+        render :edit
+    end
+  end
+
+  private #this is to ensure params is only called within this controller
+  def user_params
+    params.require(:user).permit(:username, :name, :email)
+  end
+
 end
